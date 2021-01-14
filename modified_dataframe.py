35af02c182b396caf43ac6af08c5a888284487de
ralphjstuder <51389896+ralphjstuder@users.noreply.github.com>
2021-01-14 10:55:42 -0700

    Create modified_dataframe.py

---
+++ modified_dataframe.py
@@ -0,0 +1,14 @@
+class AssetData():
+    
+    def __init__(self, filename):
+        self.filename = filename
+        self.data_frame = pd.read_csv(filename)
+        
+    def clean(self):
+        self.data_frame.fillna(method='backfill', inplace=True)
+        self.data_frame['date'] = pd.to_datetime(self.data_frame['time'].values, unit='s')
+        self.data_frame.set_index('date', inplace=True)
+        self.data_frame.drop('time', axis=1, inplace=True)
+        
+        return self.data_frame
+        
